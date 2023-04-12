2bd15c731f4fb13b2b96360f1d6f31596f7e4ec7
PedrodeP <113942638+PedrodeP@users.noreply.github.com>
2023-04-12 16:47:12 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -14,16 +14,16 @@ var bala = 70;
 
 function preload(){
   
-  heart1Img = loadImage("assets/heart_1.png")
-  heart2Img = loadImage("assets/heart_2.png")
-  heart3Img = loadImage("assets/heart_3.png")
+  heart1Img = loadImage("heart_1.png")
+  heart2Img = loadImage("heart_2.png")
+  heart3Img = loadImage("heart_3.png")
 
-  shooterImg = loadImage("assets/shooter_2.png")
-  shooter_shooting = loadImage("assets/shooter_3.png")
+  shooterImg = loadImage("shooter_2.png")
+  shooter_shooting = loadImage("shooter_3.png")
 
-  FantasminhaImg = loadImage("assets/fantasminha.png")
+  FantasminhaImg = loadImage("fantasminha.png")
 
-  bgImg = loadImage("assets/bg.jpeg")
+  bgImg = loadImage("bg.jpeg")
 
 }
 
